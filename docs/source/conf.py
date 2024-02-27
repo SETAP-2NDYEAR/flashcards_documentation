1fb75c542590d2cbb50af52bbbcee3a5114233ef
luke <50461140+0luke@users.noreply.github.com>
2024-02-27 11:26:33 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Flashcards'
+copyright = '2024, uop'
+author = 'people'
 
 release = '0.1'
 version = '0.1.0'
